a256cea23a46dc6160393f540488e030551bbbf8
Franco Bouly <fbouly@gmail.com>
2019-09-17 17:16:04 +0200

    Init

---
+++ src/components/MessageBox.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <form @submit.prevent="onFormSubmit">
+      <input 
+        type="text" 
+        placeholder="Tape ton message ici"
+        v-model="message">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      message: ''
+    }
+  },
+  methods: {
+    onFormSubmit (e) {
+      console.log('form submit')
+      this.$emit('messageSent', this.message)
+      this.message = ''
+    }
+  }
+}
+</script>
---
+++ src/views/Chat.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <UsersList/>
+    <MessagesList :messages="messages"/>
+    <MessageBox @messageSent="onMessageSent"/>
+  </div>
+</template>
+
+<script>
+import UsersList from '../components/UsersList.vue'
+import MessagesList from '../components/MessagesList.vue'
+import MessageBox from '../components/MessageBox.vue'
+
+export default {
+  data () {
+    return {
+      messages: []
+    }
+  },
+  components: {
+    UsersList,
+    MessagesList,
+    MessageBox
+  },
+  methods: {
+    onMessageSent (message) {
+      this.messages.push(message)
+    }
+  }
+}
+</script>
