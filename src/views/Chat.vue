9bad87b65bb9b6374315624905ce8a4213bd1e63
Antoine Lozach <antoine.lozach@radiofrance.com>
2020-09-15 15:44:03 +0200

    first-commit

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,57 +1,5 @@
 <template>
   <div>
-    <UsersList 
-      :users="store.users" 
-      :user="store.user"
-      :filter="filter"
-      @filterSelected="onFilterSelected"/>
-    <MessagesList 
-      :messages="store.messages" 
-      :user="store.user"
-      :users="store.users"
-      :filter="filter"
-      :typingUsers="typingUsers"/>
-    <MessageBox 
-      @messageSent="onMessageSent"
-      @typing="onTyping"/>
+    Le chat
   </div>
 </template>
-
-<script>
-import UsersList from '../components/UsersList.vue'
-import MessagesList from '../components/MessagesList.vue'
-import MessageBox from '../components/MessageBox.vue'
-
-import store from '../store'
-
-export default {
-  data () {
-    return {
-      store,
-      filter: null
-    }
-  },
-  components: {
-    UsersList,
-    MessagesList,
-    MessageBox
-  },
-  methods: {
-    onMessageSent (content) {
-      store.sendMessage(content)
-    },
-    onTyping () {
-      store.typing()
-    },
-    onFilterSelected (user) {
-      const isCurrentUser = this.filter && this.filter.username === user.username
-      this.filter = isCurrentUser ? null : user
-    }
-  },
-  computed: {
-    typingUsers () {
-      return store.users.filter((user) => user.typing)
-    }
-  }
-}
-</script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,28 +1,5 @@
 <template>
   <div>
-    <p>Please enter your name:</p>
-    <form @submit.prevent="onSubmit">
-      <input type="text" v-model="name" placeholder="Your name">
-      <button type="submit">Ok</button>
-    </form>
+    Login
   </div>
 </template>
-
-<script>
-import store from '../store'
-
-export default {
-  data () {
-    return {
-      name: ''
-    }
-  },
-  methods: {
-    onSubmit () {
-      if (this.name.length > 0) {
-        store.registerUser(this.name)
-      }
-    }
-  }
-}
-</script>
